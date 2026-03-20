717d4b1c62240f5142ec33e14c5dd594e434715e
hodge-py <105604814+hodge-py@users.noreply.github.com>
2026-03-20 09:34:50 -0600

    c

--- fundamentals.py
+++ fundamentals.py
@@ -12,45 +12,51 @@ class Fundamentals:
         self.end_date = end_date
         self.finnhub_client = finnhub.Client(api_key="d6t22lhr01qoqoiruqlgd6t22lhr01qoqoiruqm0")
         self.thresholds = {
-            "currentRatioQuarterly": (1.1, float('inf'), True),
-            "longTermDebt/equityQuarterly": (0, 1.0, False),
-            "epsGrowthTTMYoy": (15.0, float('inf'), True),
-            "netProfitMarginTTM": (10.0, float('inf'), True),
-            "pegTTM": (0.0, 1.5, False),
-            "cashPerSharePerShareQuarterly": (2.0, float('inf'), True)
-            }
+            "currentRatioQuarterly": (1.1, 'min'),
+            "epsGrowthTTMYoy": (15.0, 'min'),
+            "netProfitMarginTTM": (10.0, 'min'),
+            "longTermDebt/equityQuarterly": (1.0, 'max'),
+            "pegTTM": (1.5, 'max'),
+            "pb": (5.0, 'max') # Standard small-cap ceiling
+        }
     
     def get_fundamentals(self):
         metric = self.finnhub_client.company_basic_financials(self.ticker,'all')['metric']
         df = pd.DataFrame(
                 list(metric.items()), 
-                columns=["key", "value"]
+                columns=["metric", "value"]
         )
         ratios_df = self.finnhub_client.company_basic_financials(self.ticker,'all')['series']['annual']
         ratios_df = pd.DataFrame(list(ratios_df.items()), columns=["key", "value"])
 
         new_fund_df = pd.concat([df, ratios_df])
 
-        print(new_fund_df['key'])
-
-        new_fund_df['is_acceptable'] = (
-        (new_fund_df['currentRatioQuarterly'] >= self.thresholds['currentRatioQuarterly']) &
-        (new_fund_df['epsGrowthTTMYoy'] >= self.thresholds['epsGrowthTTMYoy']) &
-        (new_fund_df['netProfitMarginTTM'] >= self.thresholds['netProfitMarginTTM'])
-        )
-
-        conditions = [
-        (new_fund_df['epsGrowthTTMYoy'] > 25) & (new_fund_df['netProfitMarginTTM'] > 20), # High Performers
-        (new_fund_df['currentRatioQuarterly'] < 1.0),                            # Liquidity issues
-        (new_fund_df['pegTTM'] < 1.0) & (df['epsGrowthTTMYoy'] > 0),             # Undervalued Growth
-        (new_fund_df['longTermDebt/equityQuarterly'] > 1.5)                      # Debt Heavy
-        ]
         
-        choices = ['High-Efficiency Growth', 'Liquidity Risk', 'Value Play', 'Over-Leveraged']
-        
-        df['status_flag'] = np.select(conditions, choices, default='Neutral')
+        new_fund_df[['is_acceptable', 'status']] = new_fund_df.apply(lambda x: self._checkMetrics(x), axis=1)
 
         return new_fund_df
+
+
+    def _checkMetrics(self, new_fund_df):
+        metric = new_fund_df['metric']
+        value = new_fund_df['value']
+
+        if metric not in self.thresholds:
+            return new_fund_df
+        
+        limit, logic = self.thresholds[metric]
+    
+        # Logic Check
+        if logic == 'min':
+            is_ok = value >= limit
+        else:
+            is_ok = value <= limit
+            
+        # Status Labeling
+        status = "Pass" if is_ok else "Fail"
+        
+        return pd.Series([is_ok, status])
+
 
     def plot_fundamentals(self):
         pass
