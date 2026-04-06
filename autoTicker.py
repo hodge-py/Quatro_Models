01fc9c6b2080de45d27f6d0612a9ce797da61ff6
Karson Hodge <105604814+hodge-py@users.noreply.github.com>
2026-04-06 01:27:38 -0600

    c

--- autoTicker.py
+++ autoTicker.py
@@ -2,15 +2,27 @@ import papermill as pm
 import time
 import pandas as pd
 
-tickers = [
-    "HLF.TO", "CAS.TO", "DBM.TO", "CCSI", "TBLA", 
-    "LIF", "TRVG", "GT", "FA", "CCC", 
-    "STLA", "BTE", "ASIX", "SCR.TO", "CVE.TO", 
-    "IMO.TO", "BP", "SU.TO", "APOG"
+# 50 Canadian Small Cap Tickers (TSX and TSXV)
+canadian_small_caps = [
+    # Top S&P/TSX SmallCap Index Constituents
+    "MX.TO", "TVE.TO", "ATH.TO", "FVI.TO", "DML.TO", 
+    "SII.TO", "AAUC.TO", "ERO.TO", "KXS.TO", "EDR.TO",
+    "BTE.TO", "SES.TO", "GMIN.TO", "OLA.TO", "TXG.TO", 
+    "KNT.TO", "EIF.TO", "ARIS.TO", "PEY.TO", "CG.TO",
+    
+    # 2026 TSX Venture 50 & High-Growth Tickers
+    "SCZ.V", "UCU.V", "MLP.V", "AUMB.V", "TDG.V", 
+    "OMG.V", "PPP.V", "AGX.V", "NCX.V", "GGA.V",
+    "GSVR.V", "SLVR.V", "APGO.V", "ITR.V", "AGMR.V", 
+    "FLT.V", "BYN.V", "HSTR.V", "FMT.V", "ONYX.V",
+    
+    # Additional Representative Small Caps
+    "QNC.V", "GSI.V", "SLI.V", "CKG.V", "THX.V",
+    "WCP.TO", "PEY.TO", "TOU.TO", "EFN.TO", "EQB.TO"
 ]
 
 
-for i in tickers:
+for i in canadian_small_caps:
     try:
         df = pd.read_csv('Tickers.csv')
         if i in df['Tickers'].values:
